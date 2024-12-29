d397e2b7af66e75b93bad90e87150fb5620d7b83
Saeed Mehmanparast <nextartisticdevelopments@gmail.com>
2024-12-29 15:38:39 +0330

    product list items

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,18 +4,19 @@ import ProductListItem from '@/components/ProductListItem'
 import { Button, ButtonText } from '@/components/ui/button'
 import '@/global.css'
 import { Box } from '@/components/ui/box'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function HomeScreen() {
   return (
-    <>
-      <Button className="mt-20 !w-20 mx-auto">
-        <ButtonText className=" ">Text</ButtonText>
-      </Button>
-
+    <SafeAreaView>
       <FlatList
         data={products}
+        numColumns={2}
+        // data={products.splice(0, 10)}
+        contentContainerClassName="gap-2 max-w-[960px] mx-auto w-full"
+        columnWrapperClassName="gap-2"
         renderItem={({ item }) => <ProductListItem product={item} />}
       />
-    </>
+    </SafeAreaView>
   )
 }
