002b6cd40e2cbf2525ea12479b431338b16bd3d4
Saeed Mehmanparast <nextartisticdevelopments@gmail.com>
2024-12-30 15:33:41 +0330

    getting products from api

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,8 +5,13 @@ import { Button, ButtonText } from '@/components/ui/button'
 import '@/global.css'
 import { Box } from '@/components/ui/box'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { useEffect } from 'react'
+import { listProducts } from '@/api/products'
 
 export default function HomeScreen() {
+  useEffect(() => {
+    listProducts()
+  }, [])
   const { width } = useWindowDimensions()
   const numColumns = width > 700 ? 3 : 2
   // const numColumns = useBreakpointValue({
